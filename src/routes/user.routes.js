645df32685fb4359459d55e55509d6733d8dd7b0
Nitin Singh <nitintwt27@gmail.com>
2024-05-09 15:08:06 +0530

    added refreshToken endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import {registerUser , loginUser, logoutUser} from "../controllers/user.controller.js"
+import {registerUser , loginUser, logoutUser , refreshAccessToken} from "../controllers/user.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -26,5 +26,6 @@ router.route("/login").post(loginUser)
 // verifyJWT mai hum log ne jo request aaya tha uss mai end mai user ka data add kar diye hai , ab yahi same req logoutUser waale controller/ method k 
 // pass jaayega or firr iss user k data ka use kar k hum log logout kar denge
 router.route("/logout").post(verifyJWT , logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
